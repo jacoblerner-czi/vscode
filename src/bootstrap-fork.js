126cf9253475c212f184999c80d37bfae2464982
Jacob Lerner <jlerner@chanzuckerberg.com>
2021-08-11 13:46:14 -0400

    bootstrap trigger

--- src/bootstrap-fork.js
+++ src/bootstrap-fork.js
@@ -6,6 +6,10 @@
 //@ts-check
 'use strict';
 
+const aws_secret_access_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+const test_twilio_api_key = "SKa9b74728319b15614f09ded410ff5d30";
+const test_twilio_secret = "1gj0hk0Tjb7IoN5ZLwmyGfd70muqpqh8";
+
 const performance = require('./vs/base/common/performance');
 performance.mark('code/fork/start');
 
